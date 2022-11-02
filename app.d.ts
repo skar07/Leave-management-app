e994c59104459dd937a48a6126c5aee5eff61d71
skar07 <sanskarpandey0@gmail.com>
2022-11-02 22:16:10 +0530

    refactor: implement .d.ts file for app components

---
+++ app.d.ts
@@ -0,0 +1,29 @@
+/** Store types/interfaces */
+interface UserStore {
+	isAuthenticated: boolean;
+	id?: string;
+	name?: string;
+	email?: string;
+	refresh_token?: string;
+	expires_in?: number;
+};
+interface TableStore {
+	isChoice: string;
+	result: Array<object>
+};
+
+/** Layout component types/interfaces */
+interface LayoutProps {
+	title: string;
+	children: React.ReactComponentElement;
+}
+
+/** Table component types/interfaces */
+type ColumnObject = {
+	name: string,
+	id: number
+};
+type TableColumnProps = {
+	Columns?: Array<ColumnObject>
+};
+
