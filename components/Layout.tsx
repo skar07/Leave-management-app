893c54ccc64a5185108db4cf5b85f188bb252358
skar07 <sanskarpandey0@gmail.com>
2022-11-03 21:42:23 +0530

    add: implement component hierarchy for layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,11 +11,14 @@ export const Layout: React.FC<LayoutProps> = ({ title, children }) => {
 			<Head>
 				<title>{title}</title>
 			</Head>
-			<div className='flex h-screen w-screen bg-gray-100'>
+			<div className='flex flex-row h-screen w-screen bg-gray-100'>
 				<SideNav />
-				<TopNav/>
-				<TopBar />
-				{children}
+
+				<div>
+					<TopNav />
+					<TopBar />
+					{children}
+				</div>
 			</div>
 		</>
 	)
