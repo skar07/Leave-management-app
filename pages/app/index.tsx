c47b00002963f8f6836abcc7998e43e84de109e5
skar07 <sanskarpandey0@gmail.com>
2022-11-04 18:46:14 +0530

    feat: implement Listing page

---
+++ pages/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Layout } from '../../components/Layout';
+import { Filter } from '../../components/Filter';
+import { Table } from '../../components/Table';
+import styles from '../../styles/table/structure.module.css'
+
+export default function ListingPage() {
+	return (
+		<Layout title={'Your leaves'}>
+			<Table />
+		</Layout>
+	)
+}
