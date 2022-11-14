bbdc1a2787a67a70fa10341ef4c15198824099ec
skar07 <sanskarpandey0@gmail.com>
2022-11-14 22:16:26 +0530

    fix: handler functiont yping

--- components/Filter/filterItems.ts
+++ components/Filter/filterItems.ts
@@ -22,7 +22,7 @@ var date = new Date();
 var curr_date = date.getDate();
 var curr_month = date.getMonth(); //Months are zero based
 var curr_year = date.getFullYear();
-export const handleCurrentMonth = (): Handlefunctype<> => {
+export const handleCurrentMonth = () => {
 	var firstDay = curr_year + "-" + curr_month + "-" + 1;
 	var lastDay = curr_year + "-" + (curr_month + 1) + "-" + 1;
 	client
